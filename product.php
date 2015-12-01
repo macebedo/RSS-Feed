461c7b5ceabe16d47693e1405a6383e2be2d270b
AceMan <macebedo619@gmail.com>
2015-12-01 06:05:17 -0800

    Update product.php

--- product.php
+++ product.php
@@ -2,7 +2,7 @@
 	$host = "mysql1.000webhost.com";
   	$dbname = "a8773318_acebedo";
   	$dbuser = "a8773318_acebedo";
-  	$pwd = "Dublin2013";
+  	$pwd = "";
   	$dbc =0;
   	$dbc = mysqli_connect($host, $dbuser, $pwd, $dbname)
         or die ('Cannot connect to database');
